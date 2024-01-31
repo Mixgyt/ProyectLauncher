644646a63f6f30dfc42aa12aa70dae8651dc8ad6
Mixgyt <gmix990@gmail.com>
2024-01-31 11:30:55 -0600

    Agregar archivos de proyecto.

---
+++ ProyectLauncher/Views/MainView.axaml.cs
@@ -0,0 +1,68 @@
+﻿using CmlLib.Core;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Threading;
+using System.Reflection;
+using System;
+
+namespace ProyectLauncher.Views;
+
+public partial class MainView : UserControl
+{
+    MinecraftPath Path = new();
+    CMLauncher MCLauncher;
+
+    public MainView()
+    {
+        MCLauncher = new(Path);
+
+        InitializeComponent();
+        Loaded += LoadView;
+    }
+
+    public async void LoadView(object sender, RoutedEventArgs e)
+    {
+        var versions = await MCLauncher.GetAllVersionsAsync();
+        foreach (var v in versions)
+        {
+            VersionsCombo.Items.Add(v.Name);
+            if (v.IsLocalVersion)
+            {
+                VersionsCombo.SelectedItem = v.Name;
+            }
+        }
+
+        MCLauncher.FileChanged += (e) =>
+        {
+            LoadText.IsVisible = true;
+            LoadText.Text = $"{e.FileKind} | {e.FileName}  => {e.ProgressedFileCount}/{e.TotalFileCount}";
+        };
+
+        MCLauncher.ProgressChanged += (s, e) =>
+        {
+            LoadBar.Value = e.ProgressPercentage;
+        };
+    }
+
+    public void LaunchClick(object sender, RoutedEventArgs e)
+    {
+       
+        Thread process = new(() => LaunchProcess());
+        process.Name = "Launch";
+        process.IsBackground = true;
+        process.Start();
+
+    }
+
+    private async void LaunchProcess()
+    {
+        MLaunchOption Options = new()
+        {
+            MaximumRamMb = 2028,
+            JavaPath = "C:\\Program Files\\Java\\jdk-17\\bin\\javaw.exe",
+        };
+
+        var process = MCLauncher.CreateProcess($"{VersionsCombo.SelectedItem}", Options);
+        process.Start();
+    }
+}
---
+++ ProyectLauncher/Views/MainWindow.axaml.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace ProyectLauncher.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        Loaded += LoadWindow;
+    }
+
+    private void LoadWindow(object sender, RoutedEventArgs e)
+    {
+        Title = "Launcher: ";
+
+        #if Windows
+            Title += "Windows";
+        #elif OSX
+            Title += "MacOS";
+        #elif Linux
+            Title += "Linux";
+        #endif
+    }
+}
